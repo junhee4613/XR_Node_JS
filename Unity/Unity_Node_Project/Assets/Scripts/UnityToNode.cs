1355e767c5d1e9c9df2f8d9d4574139ca274793a
junhee4613 <junhee000123@naver.com>
2023-09-20 02:29:01 +0900

    Feat:packge-lock.json, Test.js, UnityToNode(주석추가)
    docs:UnityToNode.cs(문자열 수정)

--- Unity/Unity_Node_Project/Assets/Scripts/UnityToNode.cs
+++ Unity/Unity_Node_Project/Assets/Scripts/UnityToNode.cs
@@ -24,20 +24,20 @@ public class UnityToNode : MonoBehaviour
     {
         this.btnPostExample.onClick.AddListener(() =>
         {
-            var url = string.Format("0:1/2", host, port, idUri);       //URL 주소 생성        //여기 안에 {변수 순번이 들어감} Ex) 0번이 들어가면 host변수에 들어가 있는 데이터가 들어감. 중괄호를 안쓰면 그냥 0:1/2로 찍힘
+            var url = string.Format("{0}:{1}/{2}", host, port, idUri);       //URL 주소 생성        //여기 안에 {변수 순번이 들어감} Ex) 0번이 들어가면 host변수에 들어가 있는 데이터가 들어감. 중괄호를 안쓰면 그냥 0:1/2로 찍힘
             Debug.Log(url);
 
             var req = new Protocols.Packets.req_data();                         //Req 프로토콜 데이터 입력
             req.cmd = 1000;
             req.id = id;
             req.data = data;
-            var json = JsonConvert.SerializeObject(req);
+            var json = JsonConvert.SerializeObject(req);                    //json형식의 문자열로 직렬화  20230920
             Debug.Log(json);
 
             StartCoroutine(this.PostData(url, json, (raw) =>
             {
                 Protocols.Packets.common res = 
-                JsonConvert.DeserializeObject<Protocols.Packets.common>(raw);
+                JsonConvert.DeserializeObject<Protocols.Packets.common>(raw);               //역직렬화 시켜 json데이터를 c#코드로 변환 20230920
                 Debug.LogFormat("{0}, {1}", res.cmd, res.message);      //디버그로그로 서버에서 보내준 것 확인
             }));
         });
@@ -75,7 +75,7 @@ public class UnityToNode : MonoBehaviour
     private IEnumerator PostData(string url, string json, System.Action<string> callback)
     {
         var webRequest = new UnityWebRequest(url, "POST");
-        var bodyRaw = Encoding.UTF8.GetBytes(json);     //json인코딩
+        var bodyRaw = Encoding.UTF8.GetBytes(json);     //json인코딩 =>인코딩은 원본 데이터가 가지고 있는 용량을 줄이기 위해 데이터를 코드화하는 과정을 거쳐 최종적으로 용량을 압축하는 과정
 
         webRequest.uploadHandler = new UploadHandlerRaw(bodyRaw);
         webRequest.downloadHandler = new DownloadHandlerBuffer();
